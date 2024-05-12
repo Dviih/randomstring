8d533363208f3431fd2e770bb440027d1cf86488
Dviih <dviih2s@gmail.com>
2024-05-12 07:48:16 -0300

    Standard and Extended charset.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import generator from "./generator.js";
 
 export default class RandomString {
+    static charsets =  class {
+        static Standard = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        static Extended = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_=+{}[]|\";:/?.,!@#$%^&*()";
+    }
 };
