d71a814465fb01d4fa46efcf16ca3442b9b64bcb
Dviih <dviih2s@gmail.com>
2024-05-12 07:46:20 -0300

    Native generator for generating in C++.

--- src/c/generator.cpp
+++ src/c/generator.cpp
@@ -6,3 +6,17 @@
 #define NAPI_VERSION 3
 #define NODE_MODULE_VERSION 127
 
+char* nativeGenerator(size_t size, char* charset, size_t len) {
+	std::random_device randomDevice;
+	std::mt19937_64 gen(randomDevice());
+
+	std::uniform_int_distribution<> random(0, len-1);
+
+	char* data = (char*)malloc(size);
+
+	for (size_t i = 0; i < size; i++)
+		data[i] = charset[random(gen)];
+
+	return data;
+}
+
