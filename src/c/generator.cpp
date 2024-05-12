4e34adb5ef8fda766dbd80017ae163dced6b2593
Dviih <dviih2s@gmail.com>
2024-05-12 07:46:40 -0300

    wraps to Node.js and some checks to make sure it works.

--- src/c/generator.cpp
+++ src/c/generator.cpp
@@ -20,3 +20,23 @@ char* nativeGenerator(size_t size, char* charset, size_t len) {
 	return data;
 }
 
+void generator(const v8::FunctionCallbackInfo<v8::Value>& args) {
+	v8::Isolate* isolate = args.GetIsolate();
+
+	if (args.Length() < 2)
+		isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8Literal(isolate, "Missing length or charset.")));
+
+	if (!args[0]->IsUint32())
+		isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8Literal(isolate, "arg[0] is not of type int")));
+
+	if (!args[1]->IsString())
+		isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8Literal(isolate, "arg[1] is not of type string")));
+
+	size_t size = args[0]->IntegerValue(isolate->GetCurrentContext()).ToChecked();
+	if (size < 4) isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8Literal(isolate, "arg[0] must be higher than 3.")));
+
+	v8::String::Utf8Value str(isolate, args[1]);
+
+	args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, nativeGenerator(size, str.operator*(), str.length())).ToLocalChecked());
+}
+
